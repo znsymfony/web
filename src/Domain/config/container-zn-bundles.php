51b1f0bc02665ca148257da5a30a5de41dc8c959
ZnDeveloper <zndev@ya.ru>
2022-06-24 20:36:30 +0600

    upd

--- src/Domain/config/container-zn-bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'singletons' => [
-        'ZnBundle\Notify\Domain\Interfaces\Repositories\ToastrRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Symfony\ToastrRepository',
-        'ZnBundle\\Language\\Domain\\Interfaces\\Repositories\\SwitchRepositoryInterface' => 'ZnBundle\\Language\\Domain\\Repositories\\Symfony4\\SwitchRepository',
-        'ZnBundle\\Language\\Domain\\Interfaces\\Repositories\\StorageRepositoryInterface' => 'ZnBundle\\Language\\Domain\\Repositories\\Symfony4\\StorageRepository',
-    ],
-];
